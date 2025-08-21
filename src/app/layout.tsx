5ef4b0cc7eb061b0a631c2cfcac762edbd64a411
Егор Вершков <evn88fx64@gmail.com>
2025-08-22 00:42:52 +0200

    Add `@vercel/analytics` to monitor application performance and usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import {Navbar} from "@/widgets/Navbar";
+import {Analytics} from "@vercel/analytics/next";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <main className="pt-4">
             {children}
         </main>
+        <Analytics/>
         </body>
         </html>
     );
