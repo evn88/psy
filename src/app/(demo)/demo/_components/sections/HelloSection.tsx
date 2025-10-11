d7d1124553bcfc9455e83ead3d68cc5adb38a1fd
Егор Вершков <evn88fx64@gmail.com>
2025-10-11 23:52:47 +0200

    chore: clean up unused animations in HelloSection

--- src/app/(demo)/demo/_components/sections/HelloSection.tsx
+++ src/app/(demo)/demo/_components/sections/HelloSection.tsx
@@ -9,7 +9,7 @@ export const HelloSection = () => {
 
 
     useEffect(() => {
-        scope.current = createScope({root}).add(self => {
+        scope.current = createScope({root}).add(() => {
             const pulse = animate('.smlogo', {
                 scale: [{
                     to: 1.5,
@@ -38,40 +38,6 @@ export const HelloSection = () => {
                     });
                 }
             }
-
-            // animate('.smlogo', {
-            //     scale: [{
-            //         to: 1.5,
-            //         ease: 'inOut(3)',
-            //         duration: 200,
-            //     }, {to: 1, ease: spring({bounce: .7})}],
-            //     rotate: [{
-            //         to: 360,
-            //         ease: 'inOut(2)',
-            //         duration: 800,
-            //     }],
-            //     loop: true,
-            //     loopDelay: 250,
-            // })
-
-
-            // const {chars} = splitText('.logoName', {chars: true});
-
-            // animate(chars, {
-            //     scale: [{
-            //         to: 1.5,
-            //         ease: 'inOut(3)',
-            //         duration: 300,
-            //     }, {
-            //         to: 1,
-            //         ease: spring({bounce: .7})
-            //     }],
-            //     delay: function (el, i) {
-            //         return i * 100;
-            //     },
-            //     loop: true,
-            //     loopDelay: 5000,
-            // })
         })
 
         return () => scope.current.revert()
