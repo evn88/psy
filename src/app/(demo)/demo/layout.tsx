20a4d8da1fc2b67c7d8281530e0b8471cec1e0a8
Егор Вершков <evn88fx64@gmail.com>
2025-11-20 18:06:29 +0100

    refactor: replace local fonts with Google Fonts Inter in layouts and simplify font imports

--- src/app/(demo)/demo/layout.tsx
+++ src/app/(demo)/demo/layout.tsx
@@ -1,48 +1,38 @@
-import type {Metadata} from "next";
-import localFont from "next/font/local";
-import "../../globals.css";
-import {Analytics} from "@vercel/analytics/next";
-import {SpeedInsights} from "@vercel/speed-insights/next";
-import {FC} from "react";
-import {Providers} from "@/shared/Providers";
+import type { Metadata } from 'next';
+import { FC, ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+import '../../globals.css';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Providers } from '@/shared/Providers';
 
-const geistSans = localFont({
-    src: "./../../fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./../../fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+const interFont = Inter({
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-inter',
+  display: 'swap'
 });
 
 export const metadata: Metadata = {
-    title: "Demo zone - vershkov.com",
-    description: "Demo zone",
+  title: 'Demo zone - vershkov.com',
+  description: 'Demo zone'
 };
 
 type DemoLayoutType = Readonly<{
-    children: React.ReactNode;
-}>
+  children: ReactNode;
+}>;
 
-const DemoLayout: FC<DemoLayoutType> = ({children}) => {
-    return (
-        <html lang="ru">
-        <Providers>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                style={{margin: 0, padding: 0, overflow: 'hidden'}}
-            >
-            <main>
-                {children}
-            </main>
-            <Analytics/>
-            <SpeedInsights/>
-            </body>
-        </Providers>
-        </html>
-    );
-}
+const DemoLayout: FC<DemoLayoutType> = ({ children }) => {
+  return (
+    <html lang="ru">
+      <Providers>
+        <body className={interFont.className} style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
+          <main>{children}</main>
+          <Analytics />
+          <SpeedInsights />
+        </body>
+      </Providers>
+    </html>
+  );
+};
 
 export default DemoLayout;
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,56 +1,45 @@
-import type {Metadata} from "next";
-import localFont from "next/font/local";
-import "../globals.css";
-import {Navbar} from "@/widgets/Navbar";
-import {Analytics} from "@vercel/analytics/next";
-import {SpeedInsights} from "@vercel/speed-insights/next";
-import {FC} from "react";
-import {Providers} from "@/shared/Providers";
+import type { Metadata } from 'next';
+import '../globals.css';
+import { Navbar } from '@/widgets/Navbar';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { FC, ReactNode } from 'react';
+import { Providers } from '@/shared/Providers';
+import { Inter } from 'next/font/google';
 
-const geistSans = localFont({
-    src: "./../fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./../fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-inter',
+  display: 'swap'
 });
 
 export const metadata: Metadata = {
-    title: "vershkov.com",
-    description: "Psychologist",
-    appleWebApp: {
-        title: "Vershkov",
-        statusBarStyle: "default", // или "black-translucent"
-        capable: true, // добавит apple-mobile-web-app-capable
-    },
+  title: 'vershkov.com',
+  description: 'Psychologist',
+  appleWebApp: {
+    title: 'Vershkov',
+    statusBarStyle: 'default', // или "black-translucent"
+    capable: true // добавит apple-mobile-web-app-capable
+  }
 };
 
 type RootLayoutType = Readonly<{
-    children: React.ReactNode;
-}>
+  children: ReactNode;
+}>;
 
-const RootLayout: FC<RootLayoutType> = ({children}) => {
-    return (
-        <html lang="ru">
-        <Providers>
-
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-            <Navbar/>
-            <main className="pt-4 px-4 md:px-0">
-                {children}
-            </main>
-            <Analytics/>
-            <SpeedInsights/>
-            </body>
-
-        </Providers>
-        </html>
-    );
-}
+const RootLayout: FC<RootLayoutType> = ({ children }) => {
+  return (
+    <html lang="ru">
+      <Providers>
+        <body className={inter.className}>
+          <Navbar />
+          <main className="pt-4 px-4 md:px-0">{children}</main>
+          <Analytics />
+          <SpeedInsights />
+        </body>
+      </Providers>
+    </html>
+  );
+};
 
 export default RootLayout;
