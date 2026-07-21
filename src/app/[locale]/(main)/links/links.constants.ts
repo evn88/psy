85e2753d6dbafb0b23ebbba04241e4debf14a399
Anna Vershkova <a.vershkova@rusprofile.ru>
2026-07-21 15:01:14 +0200

    feat: add links hub page

---
+++ src/app/[locale]/(main)/links/links.constants.ts
@@ -0,0 +1,15 @@
+/**
+ * Все внешние адреса страницы-карты собраны здесь, чтобы их можно было
+ * заменить без правок разметки и стилей.
+ */
+export const LINKS = {
+  telegramChannel: 'https://t.me/looking_dopamine',
+  telegramCard: 'https://t.me/vershkova_psy',
+  instagram: 'https://www.instagram.com/ania_vverh/',
+  threads: 'https://www.threads.com/@ania_vverh',
+  applePodcasts: 'https://podcasts.apple.com/us/podcast/раскошные/id6784681496',
+  youtube: 'https://www.youtube.com/@Raskoshnye',
+  spotify: 'https://open.spotify.com/show/033FxPH1pzVeaJyBpxn6ze?si=Vf6szJmJRKKeJR9tSzIdfg',
+  adhdGuide: '/documents/guides/adhd-procrastination-guide.pdf',
+  autismGuide: '/documents/guides/autistic-burnout-or-depression.pdf'
+} as const;
